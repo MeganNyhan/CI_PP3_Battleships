3de558587353ba062aba31849fb9d8870a5726c9
MeganNyhan <nyhanmegan@gmail.com>
2022-01-08 17:50:09 +0000

    Add draw word function to pyhton script

--- main.py
+++ main.py
@@ -8,10 +8,23 @@ correctly_guessed_letters = []
 incorrectly_guessed_letters = []
 
 # Global variable for randomly guessed word
-randomly_chose_word = ""
+randomly_chosen_word = ""
 
 # Global variable for lives left
 lives_left = 6
 
 # Global variable for game over
 game_over = False
+
+
+def draw_word()
+    global correctly_guessed_letters
+    global randomly_chosen_word
+
+    for i in range(0, len(randomly_chosen_word)):
+        letter - randomly_chosen_word[i]
+        if letter in correctly_guessed_letters:
+            print(letter, end= " ")
+        else: 
+            print("_", end=" ")
+    print("")
