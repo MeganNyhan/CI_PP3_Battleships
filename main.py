73fcfef8b76c586bf6b499499213b93c8fb5e100
MeganNyhan <nyhanmegan@gmail.com>
2022-01-08 17:46:55 +0000

    Add constant variables to global

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import random
 import time
 
+# Global variable for correctly guessed letter
+correctly_guessed_letters = []
+
+# Global variable for incorrectly guessed letter
+incorrectly_guessed_letters = []
+
+# Global variable for randomly guessed word
+randomly_chose_word = ""
+
+# Global variable for lives left
+lives_left = 6
+
+# Global variable for game over
+game_over = False
