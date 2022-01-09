6d3e94191a5e75c6fd7878cf83445836b75b9fd0
MeganNyhan <nyhanmegan@gmail.com>
2022-01-09 16:28:10 +0000

    Add game display function and start start game function

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-#Imports the "random" to get words for the game
+#Imports the "random" to get words for the game:
 import random
 import time
 from word_list import word_list
@@ -87,3 +87,17 @@ HANGMAN = [
     """
 ]
 
+#Main Game
+def game_display():
+#This logo was taken with permission from previous student - Lee Martina:
+    print("""
+{}    {}    {}{}     {}    {}    {}}}}}    {}      {}    {}{}     {}    {}
+{}    {}   {}  {}    {}}}  {}   {}    {}   {}}}  {{{}   {}  {}    {}}}  {}
+{}{{}}{}  {}{{}}{}   {} {} {}   {}         {} {{}} {}  {}{{}}{}   {} {} {}
+{}    {}  {}    {}   {}  {{{}   {}  {{{{   {}  {}  {}  {}    {}   {}  {{{}
+{}    {}  {}    {}   {}    {}    {}}}}}    {}      {}  {}    {}   {}    {}
+        """)
+        print(HANGMAN[7])
+        name = input("Please enter your name: \n")
+        print("Welcome", name, "! Are you ready to play Hangman?")
+        print("Try to guess the Irish county before your guesses run out. You have 7 guesses!")
