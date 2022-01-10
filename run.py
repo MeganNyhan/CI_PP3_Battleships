cecb0c6f8cefa656251cea3be5c0383049ddb130
MeganNyhan <nyhanmegan@gmail.com>
2022-01-10 19:35:59 +0000

    copy and take out code into pep8 and add code back in by pasting

--- run.py
+++ run.py
@@ -2,165 +2,149 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-# Imports words from word_list.py
-from words import word_list 
-import random  # Imports random library to generate random word.
-
-"""Get word function which will get a word for the game to run.
-This function will use words from the words.py file I created."""
+import random
+from words import word_list
 
 
 def get_word():
     word = random.choice(word_list)
-    return word.upper()  # Returns word in uppercase
+    return word.upper()
 
 
 def play(word):
-    # The play function holds most of the functionality of the game.
     word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
-    tries = 7
-# This part of the function will guide the user through the game.
-    print("Are you ready to play hangman?")
+    tries = 6
+    print("Let's play Hangman!")
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
-# This is the main chunk of code here and will run until the game ends.
     while not guessed and tries > 0:
-        guess = input("Please guess a letter: ").upper()
+        guess = input("Please guess a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
-# This will check which letters have been guessed or if the letter is valid.
             if guess in guessed_letters:
-                print("You have already guessed this letter", guess)
+                print("You already guessed the letter", guess)
             elif guess not in word:
-                print("Your", guess, "is not in the word")
+                print(guess, "is not in the word.")
                 tries -= 1
                 guessed_letters.append(guess)
-            else: 
-                print("Great Guess", guess, "is correct!")
+            else:
+                print("Good job,", guess, "is in the word!")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(word) if letter == 
-                           guess]
+                indices = [i for i, letter in enumerate(word) if letter 
+                           == guess]
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
-                if "_" not in word_completion: 
+                if "_" not in word_completion:
                     guessed = True
-# This makes sure only letters are selected.
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
-                print("You already guess this letter", guess)
+                print("You already guessed the word", guess)
             elif guess != word:
                 print(guess, "is not the word.")
-                tries -= 1 
+                tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
                 word_completion = word
         else:
-            print("Not a valid guess. Please guess again.")
+            print("Not a valid guess.")
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
-# This part of the function will let the user know if their correct or not.
     if guessed:
-        print("Congratulations! You guessed the word, you Win!!")
+        print("Congrats, you guessed the word! You win!")
     else:
-        print("Sorry, you have run out of tries. The word was " + word + 
+        print("Sorry, you ran out of tries. The word was " + word + 
               ". Maybe next time!")
 
 
-# Display Hangman function displays what the user will see as the game.
 def display_hangman(tries):
-    stages = [
-                """ 
-      +------------+
-      |            |
-      |
-      |
-      |
-      |
-      |
-      +--------+
-   """,
-   """
-      +------------+
-      |            |
-      |            O
-      |
-      |
-      |
-      |
-      +--------+
-   """,
-   """
-      +------------+
-      |            |
-      |            O
-      |            |
-      |
-      |
-      |
-      +--------+
-   """,
- """
-      +------------+
-      |            |
-      |            O
-      |            |
-      |           /
-      |
-      |
-      +--------+
-   """,
-   """
-      +------------+
-      |            |
-      |            O
-      |            |
-      |           / \\
-      |
-      |
-      +--------+
-   """,
-   """
-      +------------+
-      |            |
-      |            O
-      |            |\\
-      |           / \\
-      |
-      |
-      +--------+
-   """,
-   """
-      +------------+
-      |            |
-      |            O
-      |           /|\\
-      |           / \\
-      |
-      |
-      +--------+
-   """
-]
-return stages[tries]
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
 
 
-# This is the main function that will tie the game together and allow it to 
-# function.
 def main():
     word = get_word()
     play(word)
-# This part will allow the user to replay the game if they want.
-    while input("Play Again? (Yes / No) ").upper() == "Yes":
+    while input("Play Again? (Y/N) ").upper() == "Y":
         word = get_word()
         play(word)
-# The game will no continue to run again, aslong as the user types YES.
 
-# This part of code will make sure the game will run on the command line.
+
 if __name__ == "__main__":
     main()
