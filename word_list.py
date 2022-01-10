64572130f7e86867d9e528524f7184ce4bb75fbf
MeganNyhan <nyhanmegan@gmail.com>
2022-01-10 18:30:41 +0000

    Add the get word function to game

--- word_list.py
+++
@@ -1,8 +0,0 @@
-word_list = [
-    'galway', 'leitrim', 'mayo', 'roscommon', 'sligo', 'carlow',
-    'dublin', 'kildare', 'kilkenny', 'laois', 'longford', 'louth',
-    'meath', 'offaly', 'westmeath', 'wexford', 'wicklow', 'clare',
-    'cork', 'kerry', 'limerick', 'tipperary', 'waterford', 'cavan',
-    'donegal', 'monaghan', 'antrim', 'armagh', 'down', 'fermanagh', 
-    'derry'
-    ]
