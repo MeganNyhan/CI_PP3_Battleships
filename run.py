9144c78fee6652468ab0f7442ae46b2f7211d53f
MeganNyhan <nyhanmegan@gmail.com>
2022-01-18 20:07:23 +0000

    Remove googlespreesheets work to redo tomorrow

--- run.py
+++ run.py
@@ -1,18 +1,5 @@
 import random
 from words import word_list
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('pp3_hangman')
 
 
 def get_word():
@@ -87,7 +74,7 @@ def play(word):
     if guessed:
         print("Congratulations, you guessed the CORRECT word. You WON!")
     else:
-        print("Sorry, you have run out of tries" 
+        print("Sorry, you have run out of tries"
               "the word was " + word + ". Why not try again!")
 
 
