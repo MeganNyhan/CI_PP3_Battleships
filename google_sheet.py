b23b1ebebe421ed8e3214335bfa33d4191fe0363
MeganNyhan <nyhanmegan@gmail.com>
2022-01-19 17:06:09 +0000

    Add google sheet py read function that works

--- google_sheet.py
+++ google_sheet.py
@@ -1,41 +1,28 @@
 # Google documentation
-
-from __future__ import print_function
-
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # Generate the credentials needed for code
 from google.oauth2 import service_account
 
-SERVICE_ACCOUNT_FILE = 'creds.json'
+SERVICE_ACCOUNT_FILE = 'keys.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-credentials = None
-credentials = service_account.Credentials.from_service_account_file(
+creds = None
+creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1Kv6gYod8qPLaEcLNhIDdd2mJUdZzBqcSu1wEqHsS4YQ'
 
-service = build('sheets', 'v4', credentials=credentials)
+service = build('sheets', 'v4', credentials=creds)
 
 # Call the Sheets API
 sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                            range=SAMPLE_RANGE_NAME).execute()
+                            range="user!a2:b2").execute()
 values = result.get('values', [])
+print(values)
 
-if not values:
-    print('No data found.')
-else:
-    print('Name, Major:')
-    for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
-        print('%s, %s' % (row[0], row[4]))
--- gspread_test.py
+++
@@ -1,8 +0,0 @@
-import gspread
-
-sa = gspread.service_account(filename="creds.json")
-sh = sa.open("pp3_hangman")
-
-wks = sh.worksheet("user")
-
-
