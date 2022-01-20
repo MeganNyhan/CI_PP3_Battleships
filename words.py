4ffd10fa6b26779e99d683da54b0bc3eabef806b
MeganNyhan <nyhanmegan@gmail.com>
2022-01-20 18:46:01 +0000

    Remove errors from word list

--- words.py
+++ words.py
@@ -1,31 +1,10 @@
+"""
+        Word list for the game
+"""
 word_list = [
-    'galway', 'leitrim', 'mayo', 
-    'roscommon', 
-    'sligo', 
-    'carlow',
-    'dublin', 
-    'kildare', 
-    'kilkenny', 
-    'laois', 
-    'longford', 
-    'louth',
-    'meath', 
-    'offaly', 
-    'westmeath', 
-    'wexford', 
-    'wicklow', 
-    'clare',
-    'cork', 
-    'kerry', 
-    'limerick', 
-    'tipperary', 
-    'waterford', 
-    'cavan',
-    'donegal',
-    'monaghan', 
-    'antrim', 
-    'armagh', 
-    'down', 
-    'fermanagh', 
-    'derry'
+    'galway', 'leitrim', 'mayo', 'roscommon', 'sligo', 'carlow', 'dublin',
+    'kildare', 'kilkenny', 'laois', 'longford', 'louth', 'meath', 'offaly',
+    'westmeath',  'wexford', 'wicklow', 'clare', 'cork', 'kerry', 'limerick',
+    'tipperary',  'waterford', 'cavan', 'donegal', 'monaghan', 'antrim',
+    'armagh', 'down', 'fermanagh', 'derry'
     ]
